126362e4dd32a7d8cf2b476f57f2ec2d2b2b6788
Nikhil S <nikhil@nsnnd.com>
2023-03-03 00:22:01 -0600

    drawin some lines (but in a cool way)

---
+++ src/lib/Sequence.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let xScale;
+  export let seq;
+  export let height;
+</script>
+
+<line x1="0" x2="{xScale.range()[1]}"></line>
+
+<style>
+  line {
+    stroke-width: 1px;
+    stroke: black;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Sequence from "$lib/Sequence.svelte";
+  import { scaleBand, scaleLinear } from "d3-scale";
+
+	let width = 1200;
+	let height = 1200;
+
+	let musicalTicks = 128;
+
+	let seqs = [
+		{
+			id: 1,
+			numerator: 4,
+			denominator: 4,
+			ticks: 16
+		},
+		{
+			id: 2,
+			numerator: 4,
+			denominator: 4,
+			ticks: 16
+		}
+	];
+
+  $: xScale = scaleLinear()
+    .domain([0, musicalTicks])
+    .range([0, width]);
+
+  $: yScale = scaleBand()
+		.domain(seqs.map((d) => d.id))
+    .range([0, height]);
+</script>
+
+
+
+<svg width={width} height={height}>
+	{#each seqs as seq}
+		<g transform={`translate(0, ${yScale(seq.id)})`}>
+		<Sequence
+				xScale={ xScale }
+				seq={seq}
+				height={height / seqs.length}
+		/>
+		</g>
+	{/each}
+</svg>
