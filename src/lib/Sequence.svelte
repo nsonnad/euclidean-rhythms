455044f310d1590a84cabec5e7adcceacf6f2f52
Nikhil S <nikhil@nsnnd.com>
2023-03-03 16:54:48 -0600

    rendering some algorithmic steps

--- src/lib/Sequence.svelte
+++ src/lib/Sequence.svelte
@@ -1,14 +1,32 @@
-<script>
-  export let xScale;
+<script lang="ts">
   export let seq;
-  export let height;
+
+  import { getPattern } from 'euclidean-rhythms';
+  let steps = getPattern(seq.pulses, seq.steps);
 </script>
 
-<line x1="0" x2="{xScale.range()[1]}"></line>
+
+<div class="sequence" style="grid-template-columns: repeat({seq.steps}, 1fr">
+  {#each steps as step}
+    {#if step === 1}
+      <div class="step on">on</div>
+    {:else}
+      <div class="step off">off</div>
+    {/if}
+  {/each}
+</div>
 
 <style>
-  line {
-    stroke-width: 1px;
-    stroke: black;
+  div.sequence {
+    display: grid;
+  }
+
+  div.step {
+    max-width: 60px;
+    border: 1px solid black;
+  }
+
+  div.on {
+    background-color: lightgreen;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,33 @@
 <script lang="ts">
   import Sequence from "$lib/Sequence.svelte";
-  import { scaleBand, scaleLinear } from "d3-scale";
+  // import { scaleBand, scaleLinear } from "d3-scale";
 
   let width = 1200;
   let height = 1200;
 
-  let musicalTicks = 128;
+  // let musicalTicks = 128;
 
   let seqs = [
     {
       id: 1,
-      numerator: 4,
-      denominator: 4,
+      pulses: 2,
+      steps: 4,
+      rotation: 0,
       ticks: 16
     },
     {
       id: 2,
-      numerator: 4,
-      denominator: 4,
+      pulses: 3,
+      steps: 13,
+      rotation: 0,
       ticks: 16
     }
   ];
-
-  $: xScale = scaleLinear()
-    .domain([0, musicalTicks])
-    .range([0, width]);
-
-  $: yScale = scaleBand()
-    .domain(seqs.map((d) => d.id))
-    .range([0, height]);
 </script>
 
 
-
-<svg width={width} height={height}>
+<div>
   {#each seqs as seq}
-    <g transform={`translate(0, ${yScale(seq.id)})`}>
-    <Sequence
-        xScale={ xScale }
-        seq={seq}
-        height={height / seqs.length}
-    />
-    </g>
+    <Sequence {seq} />
   {/each}
-</svg>
+</div>
