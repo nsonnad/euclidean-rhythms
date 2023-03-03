666611b458adb1eb4c70110bcccadc9ad6fdccef
Nikhil S <nikhil@nsnnd.com>
2023-03-03 16:01:22 -0600

    weird formatting stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,46 @@
-<script>
-	import Sequence from "$lib/Sequence.svelte";
+<script lang="ts">
+  import Sequence from "$lib/Sequence.svelte";
   import { scaleBand, scaleLinear } from "d3-scale";
 
-	let width = 1200;
-	let height = 1200;
+  let width = 1200;
+  let height = 1200;
 
-	let musicalTicks = 128;
+  let musicalTicks = 128;
 
-	let seqs = [
-		{
-			id: 1,
-			numerator: 4,
-			denominator: 4,
-			ticks: 16
-		},
-		{
-			id: 2,
-			numerator: 4,
-			denominator: 4,
-			ticks: 16
-		}
-	];
+  let seqs = [
+    {
+      id: 1,
+      numerator: 4,
+      denominator: 4,
+      ticks: 16
+    },
+    {
+      id: 2,
+      numerator: 4,
+      denominator: 4,
+      ticks: 16
+    }
+  ];
 
   $: xScale = scaleLinear()
     .domain([0, musicalTicks])
     .range([0, width]);
 
   $: yScale = scaleBand()
-		.domain(seqs.map((d) => d.id))
+    .domain(seqs.map((d) => d.id))
     .range([0, height]);
 </script>
 
 
 
 <svg width={width} height={height}>
-	{#each seqs as seq}
-		<g transform={`translate(0, ${yScale(seq.id)})`}>
-		<Sequence
-				xScale={ xScale }
-				seq={seq}
-				height={height / seqs.length}
-		/>
-		</g>
-	{/each}
+  {#each seqs as seq}
+    <g transform={`translate(0, ${yScale(seq.id)})`}>
+    <Sequence
+        xScale={ xScale }
+        seq={seq}
+        height={height / seqs.length}
+    />
+    </g>
+  {/each}
 </svg>
